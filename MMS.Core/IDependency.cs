0cb6ebbb257a51f0cf5411c1189e0ee62b6f32b4
Kevin Tang <kevintangtjy@hotmail.com>
2017-01-14 23:58:13 +0800

    项目初始化

---
+++ MMS.Core/IDependency.cs
@@ -0,0 +1,9 @@
+﻿namespace MMS.Core
+{
+    /// <summary>
+    /// 定义用于依赖注入的标记，继承此接口的类均可以用Autofac注入，无具体实现
+    /// </summary>
+    public interface IDependency
+    {
+    }
+}
---
+++ MMS.Core/OperateResult.cs
@@ -0,0 +1,21 @@
+﻿namespace MMS.Core
+{
+    /// <summary>
+    /// 操作结果类，包含操作结果的一些必要信息(成功与否、信息、需要反馈的数据)
+    /// </summary>
+    public class OperateResult
+    {
+        /// <summary>
+        /// 操作成功与否的标志
+        /// </summary>
+        public bool Success { get; set; }
+        /// <summary>
+        /// 操作返回的信息
+        /// </summary>
+        public string Message { get; set; }
+        /// <summary>
+        /// 操作返回的数据
+        /// </summary>
+        public object Data { get; set; }
+    }
+}
